616177bdddad3c84515f61857d34cf26967b75ea
daemon <0xDAEF0F@proton.me>
2023-07-23 12:42:54 -0600

    23_07_2023

--- exercises/move_semantics/move_semantics6.rs
+++ exercises/move_semantics/move_semantics6.rs
@@ -2,24 +2,24 @@
 // Execute `rustlings hint move_semantics6` or use the `hint` watch subcommand for a hint.
 // You can't change anything except adding or removing references.
 
-// I AM NOT DONE
-
 fn main() {
-    let data = "Rust is great!".to_string();
+    let data = "rust is great!".to_string();
 
-    get_char(data);
+    let cha = get_char(&data);
 
-    string_uppercase(&data);
+    string_uppercase(data);
+
+    println!("char is: {cha}");
 }
 
 // Should not take ownership
-fn get_char(data: String) -> char {
-    data.chars().last().unwrap()
+fn get_char(data: &str) -> char {
+    data.chars().next().unwrap()
 }
 
 // Should take ownership
-fn string_uppercase(mut data: &String) {
-    data = &data.to_uppercase();
+fn string_uppercase(mut data: String) {
+    data = data.to_uppercase();
 
     println!("{}", data);
 }
